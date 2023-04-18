0be9b038e686b0e4fe1acf3e5380166bcf40b5c8
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-04-18 14:21:59 +0000

    Fix autobuild script

--- build.sh
+++ build.sh
@@ -6,19 +6,21 @@ mkdir -p boringssl/install/lib
 
 # Build boringssl
 cd boringssl
-mkdir -p build
-cd build
-cmake -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE ..
-make
-make install DESTDIR=../install
-
-# Install protobuf
-sudo apt-get install -y protobuf-compiler
-
+    mkdir -p build
+    cd build
+        cmake -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE ..
+        make
+        make install DESTDIR=../install
 # Build the protobuf files
-cd ../proto
-protoc -I . *.proto --cpp_out=.
+cd ../../proto
+    mkdir -p protobuf
+    cd protobuf
+        wget https://github.com/protocolbuffers/protobuf/releases/download/v22.3/protoc-22.3-linux-x86_64.zip
+        unzip protoc-22.3-linux-x86_64.zip
+    cd ..
+    export PATH=$(pwd)/protobuf/bin:$PATH
+    protoc -I . *.proto --cpp_out=.
+cd ..
 
-# Build the project
-cd ../..
+# Build lilac
 make
